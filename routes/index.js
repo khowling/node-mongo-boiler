8f926f5cf32ab46daade0c447de948a85b46b99e
khowling <khowling@gmail.com>
2013-06-05 17:49:09 +0100

    update jade view

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express', online: JSON.stringify(req.online) });
+  res.render('index', { title: 'Express', online: req.online });
 };
